e8e09e42e81f0fa53c69131183d59fb3140600e0
Armine Gevorgyan <arminegevorgyan1993@gmail.com>
2020-11-04 22:05:08 +0400

    add pow first solution

--- h4/leetCode/pow.cpp
+++ h4/leetCode/pow.cpp
@@ -3,19 +3,37 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double result = 1.0;
-        while(n != 0){
-            result *= x;
-            n--;
+        if(x == 1) {
+            return 1;
         }
-        return result;
+        double result = 1.0;
+        if(n == 0) 
+        {
+            return 1;
+        }
+        else 
+        {
+            int posN = abs(n);
+            
+            while(posN != 0){
+                result *= x;
+                posN--;
+            }
+            if(n < 0) {
+                return 1 / result;
+            }
+            return result;
+        }
+   
     }
 };
 
-
 int main(){
     Solution obj;
-    std::cout << obj.myPow(2.1, 3) << '\n';
-    std::cout << obj.myPow(2.00000, 4) << '\n';
+    // std::cout << obj.myPow(2.1, 3) << '\n';
+    std::cout << obj.myPow(2.00000, 0) << '\n';
+    std::cout << obj.myPow(2.00000, -2) << '\n';
+    // std::cout << obj.myPow(1, 1122) << '\n';
     return 0;
 }
+
